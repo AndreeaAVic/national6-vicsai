465b8a042c06dfeae4a7229a2dbdfd4bc99cd870
AndreeaAVic <andreeahidigan6@gmail.com>
2021-04-06 20:55:37 +0300

    add webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+console.log('Entry point to webpack');
+import{ addPixel, addPercentage } from './utils/addUnitsType';
+
+const aRandomNumber = Math.floor(Math.random() * 20);
+console.log(aRandomNumber);
+
+const aRandomNumberWithPixel = addPixel(aRandomNumber);
+console.log(aRandomNumberWithPixel);
+
+const aRandomNumberWithPercentage = addPixel(aRandomNumber);
+console.log(aRandomNumberWithPercentage);
+
