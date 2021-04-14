f839f1b5f0790933a559208490411403f223930f
AndreeaAVic <andreeahidigan6@gmail.com>
2021-04-14 17:08:43 +0300

    adding gitignore file

--- promises.js
+++ promises.js
@@ -23,42 +23,42 @@ function thirdStep() {
 firstStep();
 
 function firstStep(number) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log('End of first step');
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log('End of first step');
 
-            resolve(number + 1);
-        }, 2000);
-    });
+      resolve(number + 1);
+    }, 2000);
+  });
 }
 
 function secondStep(param) {
-    console.log(param);
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        console.log("End of second step");
-        if (param % 2 === 1) {
-          reject("We want even param");
-        } else {
-          resolve(param / 2);
-        }
-      }, 1000);
-    });
+  console.log(param);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("End of second step");
+      if (param % 2 === 1) {
+        reject("We want even param");
+      } else {
+        resolve(param / 2);
+      }
+    }, 1000);
+  });
 }
 
 function thirdStep(finalValue) {
-    setTimeout(() => {
-        console.log('End of third step');
-        console.log('Final value', finalValue);
-    }, 500);
+  setTimeout(() => {
+    console.log('End of third step');
+    console.log('Final value', finalValue);
+  }, 500);
 }
 
 firstStep(1)
-    .then(secondStep)
-    .then(thirdStep)
-    .catch((error) => {
-        alert(`Promise chain has the following error: ${error}`);  
-    });
+  .then(secondStep)
+  .then(thirdStep)
+  .catch((error) => {
+    alert(`Promise chain has the following error: ${error}`);  
+  });
 
 // fetch example
 fetch("https://simple-json-server-scit.herokuapp.com/posts")
