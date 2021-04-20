c1b4ba8b791bee8a7bd94bf27745b5777ad6b8be
AndreeaAVic <andreeahidigan6@gmail.com>
2021-04-20 19:18:10 +0300

    clean start for 2nd part of React

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,9 @@
 import { AppLogo } from "../AppLogo/AppLogo";
 import "./Header.css";
-import { MenuButton } from "./Menu/MenuButton/MenuButton";
-import { MenuFlyout } from "./Menu/MenuFlyout/MenuFlyout";
 
 export function Header() {
   return (
     <div className="app-header">
-      <div className="app-menu">
-        <MenuButton />
-        <MenuFlyout />
-      </div>
       <AppLogo />
       <p className="app-header__title">To Do App</p>
     </div>
