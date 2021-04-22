8f2e94c729f1b21cfdb5bf5dabc49f2ace220839
AndreeaAVic <andreeahidigan6@gmail.com>
2021-04-22 23:37:59 +0300

    props and state part 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import { Header } from "./components/Header/Header";
-import { ToDoItem } from "./components/ToDoItem/ToDoItem";
-import { ToDoItemWithState } from "./components/ToDoItemWithState/ToDoItemWithState";
+// import { ToDoItem } from "./components/ToDoItem/ToDoItem";
+// import { ToDoItemWithState } from "./components/ToDoItemWithState/ToDoItemWithState";
 
 import './App.css';
+import { ToDoList } from "./components/ToDoList/ToDoList";
 
 function App() {
   return (
   <div className="App" id="app">
     <Header />
-    <ToDoItem label="task 1" checkValue={false} />
-    <ToDoItem label="task 2" checkValue={true} />
-    <ToDoItem label="task 3" checkValue={true} />
-    <ToDoItemWithState label="task 4" checkValue={true} />
+    <ToDoList />
   </div>
   );
 }
--- src/components/ToDoItem/ToDoItem.jsx
+++ src/components/ToDoItem/ToDoItem.jsx
@@ -10,6 +10,7 @@ export function ToDoItem(props) {
             <img 
                 src="https://www.flaticon.com/svg/vstatic/svg/748/748023.svg?token=exp=1618938237~hmac=cdad5d00f44b62c6cce76081ce2fa5e9" 
                 alt="trash" 
+                onClick={() => props.removeItem(props.label)}
             />
         </div>
     );
